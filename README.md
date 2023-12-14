32f2c87ee9fd2e7df78785bad8f601b79c24752a
NaDario Seays <knosence@pop-os.localdomain>
2023-12-14 16:55:43 -0500

    adds neovim config. Not complete, checking for bugs in nixosland

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # NixOS-System
+
+## Editor
+First and foremost: The most important tool in my arsenal: The Editor: Neovim 
+
+Neovim's Features:
+1. The init.lua file
+2. Lazy.nvim 
+3. ColorScheme: Catppuccin || Gruvbox
+4. Telescope
+5. TreeSitter
---
+++ User/App/nvim/init.lua
@@ -0,0 +1,70 @@
+vim.cmd("expandtab")
+vim.cmd("tabstop=2")
+vim.cmd("softtabstop=2")
+vim.cmd("shiftwidth=2 ")
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		-- or , branch = '0.1.x'
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		bulid = ":TSUpdate",
+	},
+}
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+	ensure_installed = {
+		"lua",
+		"elixir",
+		"javascript",
+		"typescript",
+		"bash",
+		"html",
+		"css",
+		"dockerfile",
+		"java",
+		"json",
+		"jsdoc",
+		"markdown",
+		"markdown_inlines",
+		"prisma",
+		"sql",
+		"toml",
+		"tsx",
+		"yuck",
+	},
+	highlight = { enable = true },
+	indent = { enable = true },
+})
+
+require("catppuccin").setup()
+vim.cmd.colorscheme("catppuccin")
