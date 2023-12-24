40245b0a92077c97a986ab615c60299312eb3d3b
knosence <nadario.seays@gmail.com>
2023-12-24 09:22:02 -0500

    bug fix: forgot ,

--- User/App/nvim/lua/plugins/lsp-config.lua
+++ User/App/nvim/lua/plugins/lsp-config.lua
@@ -14,7 +14,7 @@ return {
     end
   },
   {
-    "neovim/nvim-lspconfig"
+    "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({
