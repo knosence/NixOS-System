9d595ccc8b766778c50ba3ae0aa5a7bb86fd1a0a
knosence <nadario.seays@gmail.com>
2024-01-09 22:11:18 -0500

    feat: adds none-ls to nixvim

---
+++ User/Editors/Nixvim/none-ls.nix
@@ -0,0 +1,20 @@
+{
+  plugins.none-ls = {
+    enable = true;
+    sources = {
+      diagnostics = {
+        golangci_lint.enable = true;
+        shellcheck.enable = true;
+        statix.enable = true;
+      };
+      formatting = {
+        fantomas.enable = true;
+        gofmt.enable = true;
+        goimports.enable = true;
+        nixfmt.enable = true;
+        markdownlint.enable = true;
+        rustfmt.enable = true;
+      };
+    };
+  };
+}
