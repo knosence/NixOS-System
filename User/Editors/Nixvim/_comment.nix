7855b27deec2ab5a652e2be43cc387d0ba6e0907
knosence <nadario.seays@gmail.com>
2024-01-14 13:51:47 -0500

    feat: mv alpha to dashboard

--- User/Editors/Nixvim/_comment.nix
+++ User/Editors/Nixvim/_comment.nix
@@ -2,14 +2,8 @@
   programs.nixvim.plugins.comment-nvim = {
     enable = true;
     toggler = {
-      line = {
-        action = "cc";
-        desc = "Comments Current Line";
-      };
-      block = {
-        action = "cb";
-        desc = "Comments Block";
-      };
+      line = "<leader>cc"; 
+      block = "<leader>cb";  
     };
   };
 } 
