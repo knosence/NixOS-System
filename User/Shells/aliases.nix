6cd8616d309bb2fe85ccdcd5ee1e1605fe12ba35
knosence <nadario.seays@gmail.com>
2024-01-28 00:00:38 -0500

    feat: found better command to use fzf

--- User/Shells/aliases.nix
+++ User/Shells/aliases.nix
@@ -1,17 +1,18 @@
 {
   # Rebindings
-    ls = "colorls";
-    la = "colorls -a";
-    ".." = "cd ..";
-    "f" = "fzf";
+  ls = "colorls";
+  la = "colorls -a";
+  ".." = "cd ..";
+  "f" = "fzf --print0 | xargs -0 -o vim";
 
   # shorten system update, upgrades, and garbage collection
-    sgc = "sudo nixos-rebuild switch && home-manager switch && nix-collect-garbage --delete-older-than 5d";
-    sys = "sudo nixos-rebuild switch";
-    home = "home-manager switch";
-    gc = "nix-collect-garbage --delete-older-than 5d";
+  sgc =
+    "sudo nixos-rebuild switch && home-manager switch && nix-collect-garbage --delete-older-than 5d";
+  sys = "sudo nixos-rebuild switch";
+  home = "home-manager switch";
+  gc = "nix-collect-garbage --delete-older-than 5d";
   ## Flakes
-    "home." = "cd ~/.system && home-manager switch --flake .";
-    "sys." = "cd ~/.system && sudo nixos-rebuild switch --flake .";
-    update = "cd ~/.system && nix flake update";
+  "home." = "cd ~/.system && home-manager switch --flake .";
+  "sys." = "cd ~/.system && sudo nixos-rebuild switch --flake .";
+  update = "cd ~/.system && nix flake update";
 }
